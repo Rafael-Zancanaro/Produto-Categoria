987fd6c6859c2b6a5787b6a01502a20e099aee88
rafinha-96 <rafa10_zc@hotmail.com>
2021-08-31 17:35:19 -0300

    Parte-Back-end-Quase-concluida

---
+++ back-end/Data/Data/Context/ConnContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class ConnContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public ConnContext():base("Data Source=192.168.0.152;Initial Catalog=Produto-categoria;Persist Security Info=True;User ID=rafinha;Password=123321@")
+        {
+        }
+    }
+}
---
+++ back-end/Data/Data/Repositorys/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using Data.Context;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositorys
+{
+    public abstract class BaseRepository <M> where M : BaseModels
+    {
+        public void Create(M model)
+        {
+            using (var context = new ConnContext())
+            {
+                context.Set<M>().Add(model);
+                context.SaveChanges();
+            }
+        }
+        public List<M> Read()
+        {
+            using (var context = new ConnContext())
+            {
+                return context.Set<M>().ToList();
+            }
+        }
+        public M Read(int id)
+        {
+            using (var context = new ConnContext())
+            {
+                return context.Set<M>().Find(id);
+            }
+        }
+        public void Update(M model)
+        {
+            using (var context =new ConnContext())
+            {
+                context.Entry<M>(model).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (var context = new ConnContext())
+            {
+                context.Entry<M>(this.Read(id)).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
